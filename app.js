8304f2d5b98d341642b9774c83ef4f3a980eebd5
YRAL375 <vvoitkus@mail.ru>
2021-07-10 16:10:05 +0300

    feat: add body parser

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express()
 
-app.use("/", (req, res, next) => {
-    console.log("This Always runs")
-    next()
-})
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.use("/add-products", (req, res, next) => {
     console.log("Add products page")
-    res.send("<h1>Add products page</h1>")
+    res.send(
+        "<form action='/product' method='POST'>" +
+            "<input type='text' name='title'/>" +
+            "<button type='submit'>Add product</button>" +
+        "</form>")
+})
+
+app.use("/product", (req, res, next) => {
+    console.log(req.body)
+    res.redirect("/")
 })
 
 app.use("/", (req, res, next) => {
-    console.log("Hello from ExpressJS")
     res.send("<h1>Hello from ExpressJS!)</h1>")
 })
 
