246e246262f7be900b1d914f09651a9a2f9ddfad
YRAL375 <vvoitkus@mail.ru>
2021-07-10 19:33:04 +0300

    feat: add express router

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 
+const adminRoutes = require("./routes/admin")
+const shopRoutes = require("./routes/shop")
+
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-app.use("/add-products", (req, res, next) => {
-    console.log("Add products page")
-    res.send(
-        "<form action='/product' method='POST'>" +
-            "<input type='text' name='title'/>" +
-            "<button type='submit'>Add product</button>" +
-        "</form>")
-})
+app.use(adminRoutes)
 
-app.use("/product", (req, res, next) => {
-    console.log(req.body)
-    res.redirect("/")
-})
-
-app.use("/", (req, res, next) => {
-    res.send("<h1>Hello from ExpressJS!)</h1>")
-})
+app.use(shopRoutes)
 
 app.listen(3001)
