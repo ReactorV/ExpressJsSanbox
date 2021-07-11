17ff291921cd449c4c7d3b92652b6089d68b43b6
YRAL375 <vvoitkus@mail.ru>
2021-07-11 16:51:16 +0300

    feat: use path module for html files

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,13 @@
 const express = require("express")
+const path = require("path")
 
 const router = express.Router()
 
-router.get("/add-products", (req, res, next) => {
-    res.send(
-        "<form action='/admin/add-products' method='POST'>" +
-        "<input type='text' name='title'/>" +
-        "<button type='submit'>Add product</button>" +
-        "</form>")
+router.get("/add-product", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "../", "views", "/add-product.html"))
 })
 
-router.post("/add-products", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
     console.log(req.body)
     res.redirect("/")
 })
