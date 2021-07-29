e91ec4b604af6722b47fa42fdec9c0c58b792d9e
YRAL375 <vvoitkus@mail.ru>
2021-07-29 19:21:38 +0300

    feat: add product to array

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const path = require("path")
 
-const adminRoutes = require("./routes/admin")
+const adminData = require("./routes/admin")
 const shopRoutes = require("./routes/shop")
 
 const app = express()
@@ -12,11 +12,11 @@ app.use(express.static(path.join(__dirname, "public")))
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-app.use("/admin", adminRoutes)
+app.use("/admin", adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404page.html"))
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
 })
 
 app.listen(3001)
--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,10 @@ const express = require("express")
 const rootDir = require("../utils/path")
 const router = express.Router()
 
+const products = require("./admin")
+
 router.get("/", (req, res, next) => {
+    console.log(products)
     res.sendFile(path.join(rootDir, "views", "shop.html"))
 })
 
