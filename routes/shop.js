afe34fa43ffca490fcdd0acf1522001b35c3739d
YRAL375 <vvoitkus@mail.ru>
2021-08-01 16:35:17 +0300

    feat: outputting dynamic content with Pug

--- routes/shop.js
+++ routes/shop.js
@@ -3,13 +3,13 @@ const path = require("path")
 const express = require("express")
 
 const rootDir = require("../utils/path")
+const adminData = require("./admin")
+
 const router = express.Router()
 
-const products = require("./admin")
-
 router.get("/", (req, res, next) => {
-    console.log(products)
-    res.render("shop")
+    const products = adminData.products
+    res.render("shop", { prods: products, docTitle: "Shop" })
 })
 
 module.exports = router
