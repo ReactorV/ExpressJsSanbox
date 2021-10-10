cecae7ce2fb2ea79594851c296d11685befcec92
YRAL375 <vvoitkus@mail.ru>
2021-10-10 21:42:33 +0200

    feat: Model storing and fetching data in files

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
     constructor(titleText) {
@@ -6,10 +7,31 @@ module.exports = class Product {
     }
 
     save () {
-        products.push(this)
+        const pathToData = path.join(
+            path.dirname(process.mainModule.filename),
+            'data',
+            'products.json'
+        )
+
+        fs.readFile(pathToData, (err, fileContent) => {
+            const products = err ? [] : JSON.parse(fileContent);
+
+            products.push(this);
+            fs.writeFile(pathToData, JSON.stringify(products), (err) => {
+                console.error(err)
+            })
+        })
     }
 
-    static fetchAll () {
-        return products
-    }
+    static fetchAll (cb) {
+        const pathToData = path.join(
+            path.dirname(process.mainModule.filename),
+            'data',
+            'products.json'
+        )
+
+        fs.readFile(pathToData, (err, fileContent) => {
+            return err ? cb([]) : cb(JSON.parse(fileContent))
+        })
+    };
 }
